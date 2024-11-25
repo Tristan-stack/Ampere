4300dc17ed41b10770675ed859f8b9816109d2fe
Tristan Gerber <tristan.gerber@etu.unistra.fr>
2024-11-25 16:29:30 +0100

    dashboard page

---
+++ src/app/(protected)/dashboard/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+import React from 'react';
+import { useUser } from '@clerk/nextjs';
+
+const Dashboard = () => {
+    const { user } = useUser();
+    return (
+        <div>
+            <div>Compte : {user?.firstName}</div>
+        </div>
+    );
+};
+
+export default Dashboard;
