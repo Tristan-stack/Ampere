3d09628e693317486c7ea601f34f467935411c21
Tristan Gerber <tristan.gerber@etu.unistra.fr>
2025-02-06 15:09:39 +0100

    fix root redirect

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+
+
+export default async function Home() {
+    const { userId } = await auth();
+
+    if (userId) {
+        redirect("/batiment");
+    }
+
+    redirect("/sign-in");
+}
